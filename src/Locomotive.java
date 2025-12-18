f2573a57f8f26da9cd780826c2a08e28fdc1b0e2
tai3042006 <trannguyenanhtai30042006@gmail.com>
2025-12-19 00:41:13 +0700

    Add method to locomotive

--- src/Locomotive.java
+++ src/Locomotive.java
@@ -1,37 +1,44 @@
 
-import java.io.*;
-import java.util.*;
 
 /**
  * 
  */
 public class Locomotive {
 
-    /**
-     * Default constructor
-     */
-    public Locomotive() {
+    private String id;
+    private boolean status;
+    private double speed;
+    private Integer year;
+    public Locomotive(String id, boolean status, double speed, Integer year) {
+        this.id = id;
+        this.status = status;
+        this.speed = speed;
+        this.year = year;
     }
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	public double getSpeed() {
+		return speed;
+	}
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+	public Integer getYear() {
+		return year;
+	}
+	public void setYear(Integer year) {
+		this.year = year;
+	}
 
-    /**
-     * 
-     */
-    public String id;
-
-    /**
-     * 
-     */
-    public boolean status;
-
-    /**
-     * 
-     */
-    public double speed;
-
-    /**
-     * 
-     */
-    public Integer year;
-
-
+    
 }
