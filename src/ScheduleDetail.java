9cd7801c39d8d645fb29c8465020395afc3bc713
tai3042006 <trannguyenanhtai30042006@gmail.com>
2025-12-22 16:24:08 +0700

    add method

--- src/ScheduleDetail.java
+++ src/ScheduleDetail.java
@@ -1,50 +1,57 @@
 
-import java.io.*;
-import java.util.*;
-
-/**
- * 
- */
-public class ScheduleDetail {
-
-    /**
-     * Default constructor
-     */
-    public ScheduleDetail() {
-    }
-
-    /**
-     * 
-     */
-    public String date;
-
-    /**
-     * 
-     */
-    public LocalTime timeStart;
-
-    /**
-     * 
-     */
-    public LocalTime timeEnd;
-
-    /**
-     * 
-     */
-    public train train;
-
-    /**
-     * 
-     */
-    public route route;
-
-
-
-    /**
-     * 
-     */
-    public void Operation1() {
-        // TODO implement here
-    }
-
-}
+	
+	import java.io.*;
+	import java.time.LocalDate;
+	import java.time.LocalTime;
+	import java.util.*;
+	
+	/**
+	 * 
+	 */
+	public class ScheduleDetail {
+	    public ScheduleDetail() {
+	    }
+	    public LocalDate Date;
+	    public LocalTime timeStart;
+	    public LocalTime timeEnd;
+	    public Vehicle vehicle;
+	    public ScheduleState state;
+	    public ScheduleDetail(LocalDate Date, LocalTime timeStart, LocalTime timeEnd, ScheduleState state	) {
+	    	this.Date = Date;
+	    	this.timeEnd = timeEnd;
+	    	this.timeStart = timeStart;
+	    	this.state = state;
+	    
+	    }
+	        // TODO implement here
+		public LocalDate getDate() {
+			return Date;
+		}
+		public LocalTime getTimeStart() {
+			return timeStart;
+		}
+		public void setTimeStart(LocalTime timeStart) {
+			this.timeStart = timeStart;
+		}
+		public LocalTime getTimeEnd() {
+			return timeEnd;
+		}
+		public void setTimeEnd(LocalTime timeEnd) {
+			this.timeEnd = timeEnd;
+		}
+		public Vehicle getVehicle() {
+			return vehicle;
+		}
+		public void setVehicle(Vehicle vehicle) {
+			this.vehicle = vehicle;
+		}
+		public ScheduleState getState() {
+			return state;
+		}
+		public void setState(ScheduleState state) {
+			this.state = state;
+		}	
+		public ScheduleState isRunning() {
+			return state = ScheduleState.RUNNING;
+		}
+	    }
